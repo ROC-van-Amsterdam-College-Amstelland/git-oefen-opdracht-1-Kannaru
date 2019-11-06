18cb8ee25a23da2ef7538fd2c97a27320018b01b
unknown <you@example.com>
2019-11-06 11:22:29 +0100

    date fixed+button half af

---
+++ pages/contacten/script.js
@@ -0,0 +1,26 @@
+window.onload = setInterval(clock,1000);
+function clock()
+{
+    var d = new Date();
+    var date = d.getDate();
+    var year = d.getFullYear();
+    var month = d.getMonth();
+    var monthArr = ["January", "February","March", "April", "May", "June", "July", "August", "September", "October", "November","December"];
+    month = monthArr[month];
+    document.getElementById("date").innerHTML=date+" "+month+", "+year;
+}
+
+
+var whichButton = "";
+var counter = 0;                         
+var counter2 = 0;                       
+$("button").on("click", function(){     
+  whichButton = this.id;                   
+  if (whichButton === "button1") {        
+    counter++; 
+    $("#span1").html(counter);
+  } else {
+    counter2++
+    $("#span2").html(counter2);
+  }
+});
